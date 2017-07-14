314571eb37534079d65768677d758e9bd7975855
Jeff Escalante <escalantejeff@gmail.com>
2017-07-14 12:55:16 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const pageId = require('spike-page-id')
+const env = process.env.NODE_ENV
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  module: {
+    rules: [{
+      test: /\.scss/,
+      use: [
+        { loader: 'source-loader' },
+        { loader: 'sass-loader', options: { _spikeExtension: 'css' } }
+      ]
+    }]
+  },
+  reshape: htmlStandards({
+    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+    minify: env === 'production'
+  }),
+  postcss: cssStandards({
+    minify: env === 'production',
+    warnForDuplicates: env !== 'production'
+  }),
+  babel: jsStandards()
+}
